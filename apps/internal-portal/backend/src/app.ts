d128f976cc93765669e0fe25c0a5a9a1e9291e60
Sven Johansson <sven.johansson@iteam.se>
2025-08-04 07:25:09 +0200

    prettier-fixup - @onecore/internal-portal-backend

--- apps/internal-portal/backend/src/app.ts
+++ apps/internal-portal/backend/src/app.ts
@@ -22,10 +22,8 @@ const CONFIG: Partial<session.opts<DefaultState, DefaultContext, unknown>> = {
   overwrite: true /** (boolean) can overwrite or not (default true) */,
   httpOnly: true /** (boolean) httpOnly or not (default true) */,
   signed: false /** (boolean) signed or not (default true) */,
-  rolling:
-    true /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */,
-  renew:
-    false /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/,
+  rolling: true /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */,
+  renew: false /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/,
   secure: false /** (boolean) secure cookie*/,
   sameSite: undefined,
 }
