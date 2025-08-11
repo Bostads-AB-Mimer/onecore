216d6da0f53cb2165818145c57696852997b1fad
henrikhenrikpersson <henrik.persson@iteam.se>
2025-08-11 11:43:46 +0200

    Add eslint (#3)
    Co-authored-by: Henrik Persson <henrik.persson@iteam.com>

---
+++ services/property/eslint.config.mjs
@@ -0,0 +1,33 @@
+import js from '@eslint/js'
+import globals from 'globals'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  { ignores: ['dist'] },
+  {
+    extends: [js.configs.recommended, ...tseslint.configs.recommended],
+    files: ['**/*.{ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: globals.browser,
+    },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+    },
+  },
+  [
+    {
+      rules: {
+        '@typescript-eslint/no-empty-object-type': 'off',
+      },
+    },
+  ]
+)
