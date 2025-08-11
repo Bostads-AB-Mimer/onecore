aa8ebdac233e3b6aa89e89521a04b2e693a97e7c
Andreas Lundqvist <momentiris@gmail.com>
2025-08-11 16:43:29 +0200

    chore: script db initialization

---
+++ scripts/db/initialize.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Running database initialization..."
+
+docker exec -i onecore-sql bash -c '/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" -C' <<-EOSQL
+	CREATE DATABASE [tenants-leases];
+	CREATE DATABASE [tenants-leases-test];
+EOSQL
+
+echo "Database initialization completed!"
