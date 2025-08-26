93057ed62287d8d19208f77e904ec168e377ce1e
Linda Almqvist <linda.almqvist@prototyp.se>
2025-08-26 15:09:55 +0200

    disable creation of application for non scored listings

--- apps/internal-portal/frontend/src/pages/ParkingSpaces/index.tsx
+++ apps/internal-portal/frontend/src/pages/ParkingSpaces/index.tsx
@@ -207,7 +207,7 @@ const getActionColumns = (): Array<GridColDef<ListingWithOffer>> => {
         />,
         <CreateApplicantForListing
           key={1}
-          disabled={row.status !== ListingStatus.Active}
+          disabled={row.status !== ListingStatus.Active || row.rentalRule === 'NON_SCORED'}
           listing={row}
         />,
       ],
