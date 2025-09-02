c7409904a5edd52285ba4144858d6ebbee0a424f
Sven Johansson <sven.johansson@iteam.se>
2025-09-02 22:25:47 +0200

    internal-portal-backend: Normalized and trimmed build configuration

--- apps/internal-portal/backend/test/middlewares/error-handler.test.ts
+++ apps/internal-portal/backend/test/middlewares/error-handler.test.ts
@@ -1,16 +1,26 @@
 import Koa from 'koa'
 import request from 'supertest'
-import errorHandler from './error-handler'
+import errorHandler from '@/middlewares/error-handler'
 
 describe('errorHandler', () => {
   let app: Koa<Koa.DefaultState, Koa.DefaultContext>
+  let errSpy: jest.SpyInstance
+
+  beforeAll(() => {
+    errSpy = jest.spyOn(console, 'error').mockImplementation(() => {})
+  })
+
+  afterAll(() => {
+    errSpy.mockRestore()
+  })
+
   beforeEach(() => {
     app = new Koa()
     app.use(errorHandler())
   })
 
   it('catches errors and passes the error message along to the response', async () => {
-    app.use(async (ctx, next) => {
+    app.use(async (_ctx, _next) => {
       throw new Error('I should be caught')
     })
     app.use(async (ctx, next) => {
