40dc27691c05f650197be37f83cbaf91da785144
Sven Johansson <sven.johansson@iteam.se>
2025-09-08 17:04:59 +0200

    @onecore/communication: eslint config inheritance

--- services/communication/eslint.config.mjs
+++ services/communication/eslint.config.mjs
@@ -1,56 +1,11 @@
 import { defineConfig } from 'eslint/config';
-import typescriptEslint from 'typescript-eslint';
-import nPlugin from 'eslint-plugin-n';
-import js from '@eslint/js';
-import prettier from 'eslint-config-prettier';
-import importPlugin from 'eslint-plugin-import';
-import globals from 'globals';
+
+import onecoreBase from '../../eslint.config.mjs'
 
 export default defineConfig([
+  onecoreBase,
   {
     files: ['**/*.{js,ts,mjs,cjs}'],
-    languageOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      parser: typescriptEslint.parser,
-      parserOptions: {
-        project: ['**/tsconfig.json'],
-      },
-      globals: {
-        ...globals.node,
-        ...globals.jest,
-      },
-    },
-    plugins: {
-      '@typescript-eslint': typescriptEslint.plugin,
-      import: importPlugin,
-      n: nPlugin,
-    },
-    settings: {
-      node: {
-        tryExtensions: ['.js', '.ts'],
-      },
-    },
-    linterOptions: {
-      reportUnusedDisableDirectives: true,
-    },
-    rules: {
-      ...js.configs.recommended.rules,
-      ...typescriptEslint.configs.recommended.rules,
-      ...prettier.rules,
-      ...importPlugin.configs.typescript.rules,
-      ...nPlugin.configs.recommended.rules,
-      '@typescript-eslint/no-explicit-any': 'warn',
-      'n/no-unsupported-features/es-syntax': [
-        'error',
-        {
-          ignores: ['modules'],
-        },
-      ],
-      'no-useless-catch': 'warn',
-      'no-unused-vars': 'warn',
-      'n/no-missing-import': 'off'
-    },
   },
   {
     files: ['src/**/*.test.ts'],
