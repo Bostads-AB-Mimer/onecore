6a6d932bf3146b5e1e6fedabea3bff180fa75e0d
Sven Johansson <sven.johansson@iteam.se>
2025-09-08 14:38:43 +0200

    infra: Auto-merge version conflicts in package.json

---
+++ scripts/git/merge-semver.js
@@ -0,0 +1,50 @@
+#!/usr/bin/env node
+
+/**
+ * This script is used by git, with the "semver" merge strategy enabled
+ * (use `npm run git:configure` or `npm run dev:init` to enabled)
+ *
+ * Compares the version attribute of package.json from conflicting versions
+ * and picks the highest semver number.
+ */
+const fs = require('fs')
+const semver = require('semver')
+
+const readPkgJson = (path) => {
+  try {
+    return {
+      path,
+      json: JSON.parse(fs.readFileSync(path, 'utf8')),
+    }
+  } catch {
+    return null
+  }
+}
+
+// Read ancestor, our version and their version of package.json
+const [, , basePath, oursPath, theirsPath] = process.argv
+const files = [basePath, oursPath, theirsPath].map(readPkgJson)
+
+const [, ourFile] = files
+
+// Sort versions and pick the highest semver version number
+const versions = files
+  .filter(Boolean)
+  .map((f) => f.json.version)
+  .filter(Boolean)
+  .sort((a, b) => {
+    const va = semver.valid(a)
+    const vb = semver.valid(b)
+    if (va && vb) return semver.gt(va, vb) ? -1 : 1
+    if (va) return -1
+    if (vb) return 1
+    return 0
+  })
+const highest = versions[0]
+
+// Set version and save updated file
+ourFile.json.version = highest
+fs.writeFileSync(ourFile.path, JSON.stringify(ourFile.json, null, 2), 'utf8')
+
+// Make sure we exit with code 0 to signal to git that the operation was successful
+process.exit(0)
