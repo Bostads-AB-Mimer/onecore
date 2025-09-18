2c9678f9f3a66288b0b5bfb2436711fce80a72e6
lindblomdavid <daavidlindblom@gmail.com>
2025-09-18 15:12:23 +0200

    added index and app.tsx files

---
+++ services/keys/src/app.tsx
@@ -0,0 +1,39 @@
+import Koa from 'koa'
+import bodyParser from 'koa-bodyparser'
+import cors from '@koa/cors'
+import { logger, loggerMiddlewares } from '@onecore/utilities'
+
+//import api from './api'
+//import errorHandler from './middlewares/error-handler'
+
+
+const app = new Koa()
+
+import { koaSwagger } from 'koa2-swagger-ui'
+
+app.use(cors())
+
+app.use(
+  koaSwagger({
+    routePrefix: '/swagger',
+    swaggerOptions: {
+      url: '/swagger.json',
+    },
+  })
+)
+
+app.on('error', (err) => {
+  logger.error(err)
+})
+
+//app.use(errorHandler())
+
+app.use(bodyParser())
+
+// Log the start and completion of all incoming requests
+app.use(loggerMiddlewares.pre)
+app.use(loggerMiddlewares.post)
+
+//app.use(api.routes())
+
+export default app
