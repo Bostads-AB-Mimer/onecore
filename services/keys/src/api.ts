53a3f9b8a25f8925eadaa62a1c8c5a4e3c1fa7ef
Sebastian Salas <S3salas@gmail.com>
2025-09-19 13:05:20 +0200

    editing swagger into api.ts

--- services/keys/src/api.ts
+++ services/keys/src/api.ts
@@ -1,9 +1,13 @@
 import KoaRouter from '@koa/router'
 import { routes as keyRoutes } from './services/key-service'
+import { routes as swaggerRoutes } from './services/swagger'   // <— add this
+
 
 
 const router = new KoaRouter()
 
 keyRoutes(router)
+swaggerRoutes(router)                                         // <— and this
+
 
 export default router
