b6e0f619b76866868bb52cb80e95dd9e8babfec5
albinrm <albin.rm@hotmail.com>
2025-09-19 11:33:29 +0200

    add key loans routes to the keys service API

--- services/keys/src/api.ts
+++ services/keys/src/api.ts
@@ -1,10 +1,9 @@
 import KoaRouter from '@koa/router'
 import { routes as keyRoutes } from './services/key-service'
-import { routes as keyLoanRoutes } from './services/key-service/routes/key-loans'  // Add this line
 
 const router = new KoaRouter()
 
 keyRoutes(router)
-keyLoanRoutes(router) 
+
 
 export default router
