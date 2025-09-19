abcd5888811b8767e7843e0bf2e10c9a10a3645b
Sebastian Salas <S3salas@gmail.com>
2025-09-19 10:03:59 +0200

    activating api, crud operations working for keys table, fixed uppercase problem

--- services/keys/src/app.ts
+++ services/keys/src/app.ts
@@ -2,11 +2,7 @@ import Koa from 'koa'
 import bodyParser from 'koa-bodyparser'
 import cors from '@koa/cors'
 import { logger, loggerMiddlewares } from '@onecore/utilities'
-
-//import api from './api'
-//import errorHandler from './middlewares/error-handler'
-
-
+import api from './api'
 const app = new Koa()
 
 import { koaSwagger } from 'koa2-swagger-ui'
@@ -16,9 +12,7 @@ app.use(cors())
 app.use(
   koaSwagger({
     routePrefix: '/swagger',
-    swaggerOptions: {
-      url: '/swagger.json',
-    },
+    swaggerOptions: { url: '/swagger.json' },
   })
 )
 
@@ -26,14 +20,11 @@ app.on('error', (err) => {
   logger.error(err)
 })
 
-//app.use(errorHandler())
-
 app.use(bodyParser())
-
-// Log the start and completion of all incoming requests
 app.use(loggerMiddlewares.pre)
 app.use(loggerMiddlewares.post)
 
-//app.use(api.routes())
+app.use(api.routes())
+app.use(api.allowedMethods())
 
 export default app
