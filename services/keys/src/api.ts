05665da208c23c82415ccaca32d186907103b648
lindblomdavid <daavidlindblom@gmail.com>
2025-09-19 12:53:59 +0200

    added swagger for key-systems in microservice

--- services/keys/src/api.ts
+++ services/keys/src/api.ts
@@ -1,9 +1,10 @@
 import KoaRouter from '@koa/router'
 import { routes as keyRoutes } from './services/key-service'
+import { routes as swaggerRoutes } from './services/swagger'
 
 const router = new KoaRouter()
 
 keyRoutes(router)
-
+swaggerRoutes(router)
 
 export default router
