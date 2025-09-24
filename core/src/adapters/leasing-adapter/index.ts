8d9b37bed58fdd4eef22955347c2c0bafab9009b
Johanna Månsson Grahn <johanna.grahn@iteam.se>
2025-09-24 11:14:31 +0200

    Feat:UTH-237 Route to Get Note of interests / Applicants should also … (#48)
    …get Rental Object for the listing
    
    Rental object is needed as well now that we dont save rental object info to db

--- core/src/adapters/leasing-adapter/index.ts
+++ core/src/adapters/leasing-adapter/index.ts
@@ -20,6 +20,7 @@ import { z } from 'zod'
 import { AdapterResult } from './../types'
 import config from '../../common/config'
 import { getListingByListingId } from './listings'
+import { getParkingSpaceByCode } from './rental-objects'
 
 //todo: move to global config or handle error statuses in middleware
 axios.defaults.validateStatus = function (status) {
@@ -335,6 +336,19 @@ const getApplicantsAndListingByContactCode = async (
     for (const applicant of applicantsResponse) {
       const listingResponse = await getListingByListingId(applicant.listingId)
       if (listingResponse) {
+        //also get rental object for the listing
+        const rentalObjectResponse = await getParkingSpaceByCode(
+          listingResponse.rentalObjectCode
+        )
+        if (rentalObjectResponse.ok) {
+          listingResponse.rentalObject = rentalObjectResponse.data
+        } else {
+          logger.info(
+            { rentalObjectCode: listingResponse.rentalObjectCode },
+            'getApplicantsAndListingByContactCode: Could not fetch rental object for listing'
+          )
+        }
+
         applicantsAndListings.push({ applicant, listing: listingResponse })
       }
     }
