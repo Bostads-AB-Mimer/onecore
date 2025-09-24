2be5ea5363976ab3607c65f60f63df0cde334d92
Sven Johansson <sven.johansson@iteam.se>
2025-09-24 17:56:57 +0200

    Fix DB init script

--- scripts/db/initialize.sh
+++ scripts/db/initialize.sh
@@ -2,11 +2,11 @@
 
 echo "Running database initialization..."
 
-docker exec -i onecore-sql bash -c '/opt/mssql-tools18/bin/sqlcmd -S localhost -U SA -P "$MSSQL_SA_PASSWORD" -C' <<-EOSQL
-	CREATE DATABASE [tenants-leases];
-	CREATE DATABASE [tenants-leases-test];
-	CREATE DATABASE [property-info];
-  CREATE DATABASE [economy];
+docker exec -i onecore-sql bash -c "/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -P \"s3cr3t_p455w0rd\" -C" <<-EOSQL
+    CREATE DATABASE [tenants-leases];
+    CREATE DATABASE [tenants-leases-test];
+    CREATE DATABASE [property-info];
+    CREATE DATABASE [economy];
 EOSQL
 
 echo "Database initialization completed!"
