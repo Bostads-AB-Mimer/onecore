f8cf848a57ee1615362c9c995c28bd363381a36f
Henrik Persson <henrik.persson@iteam.com>
2025-09-24 14:57:24 +0200

    Handle different types of inkasso-files

--- services/economy/src/scripts/import-debt-collection.ts
+++ services/economy/src/scripts/import-debt-collection.ts
@@ -1,72 +1,137 @@
 import { logger } from '@onecore/utilities'
 import config from '../common/config'
 import path from 'node:path'
-import { enrichRentCases } from '../services/debt-collection-service/service'
-import SftpClient from 'ssh2-sftp-client'
+import {
+  enrichBalanceCorrections,
+  enrichOtherInvoices,
+  enrichRentInvoices,
+  EnrichResponse,
+} from '../services/debt-collection-service/service'
+import SftpClient, { FileInfo } from 'ssh2-sftp-client'
 
 const importSftpConfig: SftpClient.ConnectOptions = {
-  host: config.debtCollection.xledgerSftp.host,
-  username: config.debtCollection.xledgerSftp.username,
-  password: config.debtCollection.xledgerSftp.password,
-  port: config.debtCollection.xledgerSftp.port ?? 22,
-  debug: console.log,
-  algorithms: config.debtCollection.xledgerSftp.useSshDss
+  host: config.debtCollection.xledger.sftp.host,
+  username: config.debtCollection.xledger.sftp.username,
+  password: config.debtCollection.xledger.sftp.password,
+  port: config.debtCollection.xledger.sftp.port ?? 22,
+  algorithms: config.debtCollection.xledger.sftp.useSshDss
     ? {
         serverHostKey: ['ssh-dss'],
       }
     : undefined,
-}
-
-const exportSftpConfig: SftpClient.ConnectOptions = {
-  host: config.debtCollection.sergelSftp.host,
-  username: config.debtCollection.sergelSftp.username,
-  password: config.debtCollection.sergelSftp.password,
-  port: config.debtCollection.sergelSftp.port ?? 22,
   debug: console.log,
 }
 
-if (!config.debtCollection.xledgerSftp.directory) {
+const exportSftpConfig: SftpClient.ConnectOptions = {
+  host: config.debtCollection.sergel.sftp.host,
+  username: config.debtCollection.sergel.sftp.username,
+  password: config.debtCollection.sergel.sftp.password,
+  port: config.debtCollection.sergel.sftp.port ?? 22,
+  debug: console.log,
+}
+
+if (!config.debtCollection.xledger.rentInvoicesDirectory) {
   throw new Error(
-    'Debt collection Xledger sftp config is missing directory property'
+    'Debt collection Xledger sftp config is missing rent invoices directory property'
   )
 }
-if (!config.debtCollection.sergelSftp.directory) {
+
+if (!config.debtCollection.xledger.otherInvoicesDirectory) {
+  throw new Error(
+    'Debt collection Xledger sftp config is missing other invoices directory property'
+  )
+}
+
+if (!config.debtCollection.xledger.balanceCorrectionsDirectory) {
+  throw new Error(
+    'Debt collection Xledger sftp config is missing other balance corrections directory property'
+  )
+}
+
+if (!config.debtCollection.sergel.directory) {
   throw new Error(
     'Debt collection Sergel sftp config is missing directory property'
   )
 }
-const importDirectory = config.debtCollection.xledgerSftp.directory
-const exportDirectory = config.debtCollection.sergelSftp.directory
 
-const getCsvFilenames = async (client: SftpClient) => {
-  const files = await client.list(importDirectory, (fileInfo) => {
-    return fileInfo.name.toLowerCase().endsWith('.csv')
-  })
+const exportDirectory = config.debtCollection.sergel.directory
+const rentInvoicesDirectory =
+  config.debtCollection.xledger.rentInvoicesDirectory
+const balanceCorrectionsDirectory =
+  config.debtCollection.xledger.balanceCorrectionsDirectory
+const otherInvoicesDirectory =
+  config.debtCollection.xledger.otherInvoicesDirectory
 
-  return files.map((fileInfo) => fileInfo.name)
+type DebtCollectionFile = {
+  type: 'rentInvoice' | 'otherInvoice' | 'balanceCorrection'
+  directory: string
+  fileName: string
 }
 
-const readFile = async (client: SftpClient, fileName: string) => {
-  const contents = await client.get(path.join(importDirectory, fileName))
+const getDebtCollectionFiles = async (client: SftpClient) => {
+  const filter = (fileInfo: FileInfo) =>
+    fileInfo.name.toLowerCase().endsWith('.csv')
+
+  const rentInvoiceFiles = await client.list(rentInvoicesDirectory, filter)
+  const balanceCorrectionFiles = await client.list(
+    balanceCorrectionsDirectory,
+    filter
+  )
+  const otherInvoiceFiles = await client.list(otherInvoicesDirectory, filter)
+
+  return [
+    ...rentInvoiceFiles.map(
+      (fileInfo): DebtCollectionFile => ({
+        type: 'rentInvoice',
+        directory: rentInvoicesDirectory,
+        fileName: fileInfo.name,
+      })
+    ),
+    ...otherInvoiceFiles.map(
+      (fileInfo): DebtCollectionFile => ({
+        type: 'otherInvoice',
+        directory: otherInvoicesDirectory,
+        fileName: fileInfo.name,
+      })
+    ),
+    ...balanceCorrectionFiles.map(
+      (fileInfo): DebtCollectionFile => ({
+        type: 'balanceCorrection',
+        directory: balanceCorrectionsDirectory,
+        fileName: fileInfo.name,
+      })
+    ),
+  ]
+}
+
+const readFile = async (client: SftpClient, filePath: string) => {
+  const contents = await client.get(filePath)
   return contents.toString()
 }
 
-const markCsvFileAsCompleted = async (client: SftpClient, fileName: string) => {
-  await client.rename(
-    path.join(importDirectory, fileName),
-    path.join(importDirectory, fileName.replace(/\.csv/i, '.csv-imported'))
-  )
+const markCsvFileAsCompleted = async (client: SftpClient, filePath: string) => {
+  await client.rename(filePath, filePath.replace(/\.csv/i, '.csv-imported'))
 }
 
-const markCsvFileAsFailed = async (client: SftpClient, fileName: string) => {
-  await client.rename(
-    path.join(importDirectory, fileName),
-    path.join(importDirectory, fileName.replace(/\.csv/i, '.csv-failed'))
-  )
+const markCsvFileAsFailed = async (client: SftpClient, filePath: string) => {
+  await client.rename(filePath, filePath.replace(/\.csv/i, '.csv-failed'))
 }
 
 const getExportFilePath = (fileName: string) => {
   return path.join(exportDirectory, fileName.replace(/\.csv/i, '.txt'))
+}
+
+const createBufferForSergel = (contents: string) => {
+  return Buffer.from(contents.replaceAll('\n', '\r\n'), 'latin1')
+}
+
+const enrichers: Record<
+  DebtCollectionFile['type'],
+  (s: string) => Promise<EnrichResponse>
+> = {
+  balanceCorrection: enrichBalanceCorrections,
+  rentInvoice: enrichRentInvoices,
+  otherInvoice: enrichOtherInvoices,
 }
 
 const processDebtCollectionFiles = async () => {
@@ -77,32 +142,47 @@ const processDebtCollectionFiles = async () => {
     await importClient.connect(importSftpConfig)
     await exportClient.connect(exportSftpConfig)
 
-    const csvFileNames = await getCsvFilenames(importClient)
-    logger.info(csvFileNames)
+    const debtCollectionFiles = await getDebtCollectionFiles(importClient)
+    logger.info(debtCollectionFiles)
 
-    for (const csvFileName of csvFileNames) {
-      const fileContents = await readFile(importClient, csvFileName)
-      const response = await enrichRentCases(fileContents)
+    for (const debtCollectionFile of debtCollectionFiles) {
+      const fileContents = await readFile(
+        importClient,
+        path.join(debtCollectionFile.directory, debtCollectionFile.fileName)
+      )
+
+      const response = await enrichers[debtCollectionFile.type](fileContents)
+
       if (!response.ok) {
-        logger.error(response.error, `Failed to process file ${csvFileName}`)
-        await markCsvFileAsFailed(importClient, csvFileName)
+        logger.error(
+          response.error,
+          `Failed to process file ${debtCollectionFile}`
+        )
+        await markCsvFileAsFailed(
+          importClient,
+          path.join(debtCollectionFile.directory, debtCollectionFile.fileName)
+        )
         continue
       }
 
-      const fp = getExportFilePath(csvFileName)
+      const fp = getExportFilePath(debtCollectionFile.fileName)
+      const buffer = createBufferForSergel(response.file)
 
-      await exportClient.put(Buffer.from(response.file, 'latin1'), fp, {
+      await exportClient.put(buffer, fp, {
         writeStreamOptions: {
           flags: 'w',
           mode: 0o666,
         },
       })
 
-      await markCsvFileAsCompleted(importClient, csvFileName)
+      await markCsvFileAsCompleted(
+        importClient,
+        path.join(debtCollectionFile.directory, debtCollectionFile.fileName)
+      )
     }
   } catch (err) {
     logger.error(err)
-    throw new Error('SFTP : ' + JSON.stringify(err))
+    throw err
   } finally {
     await importClient.end()
     await exportClient.end()
