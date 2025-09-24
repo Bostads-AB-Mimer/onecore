0084e81d0f0b3d17dbc25923bd3e6ac5ba31cd04
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2025-09-24 09:58:31 +0200

    UTH-238: Unpublish listing (#38)
    * leasing: removing seemingly weird case constraint until i find out why its there
    * internal-portal-be: add unpublish route
    * internal-portal: unpublish and add comment
    * internal-portal: detail view btn on historical listings
    * Fixes to build error problems
    
    ---------
    
    Co-authored-by: Johanna Månsson Grahn <johanna.grahn@iteam.se>

--- apps/internal-portal/backend/src/services/leasing-service/listings.ts
+++ apps/internal-portal/backend/src/services/leasing-service/listings.ts
@@ -1,6 +1,7 @@
 import KoaRouter from '@koa/router'
+import { generateRouteMetadata, logger } from '@onecore/utilities'
+
 import * as coreAdapter from './adapters/core-adapter'
-import { generateRouteMetadata } from '@onecore/utilities'
 
 export const routes = (router: KoaRouter) => {
   router.post('(.*)/listings', async (ctx) => {
@@ -125,6 +126,40 @@ export const routes = (router: KoaRouter) => {
         ...metadata,
       }
     }
+  })
+
+  router.put('(.*)/listings/:listingId/unpublish', async (ctx) => {
+    const metadata = generateRouteMetadata(ctx)
+    const unpublish = await coreAdapter.closeListing(
+      Number(ctx.params.listingId)
+    )
+
+    if (!unpublish.ok) {
+      ctx.status = 500
+      ctx.body = {
+        ...metadata,
+      }
+
+      logger.error({ error: unpublish.err }, 'Failed to close listing')
+      return
+    }
+
+    const addComment = await coreAdapter.addComment(
+      { targetType: 'listing', targetId: Number(ctx.params.listingId) },
+      {
+        authorId: ctx.session?.account.username,
+        authorName: ctx.session?.account.name,
+        comment: 'Bilplatsannons avpublicerad',
+        type: 'COMMENT',
+      }
+    )
+
+    if (!addComment.ok) {
+      logger.error({ error: addComment.err }, 'Failed to add comment')
+    }
+
+    ctx.status = 200
+    ctx.body = metadata
   })
 
   // /listings/with-applicants
