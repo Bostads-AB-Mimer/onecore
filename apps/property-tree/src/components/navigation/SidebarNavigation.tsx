c13a05f8e7e1e6a53756432d746f230a9ce167a9
Simon Kropp <simon.kropp@prototyp.se>
2025-09-25 10:29:27 +0200

    re-add sidebar collapsible for companies forn ow

--- apps/property-tree/src/components/navigation/SidebarNavigation.tsx
+++ apps/property-tree/src/components/navigation/SidebarNavigation.tsx
@@ -1,3 +1,8 @@
+import {
+  Collapsible,
+  CollapsibleContent,
+  CollapsibleTrigger,
+} from '@radix-ui/react-collapsible'
 import { CompanyList } from './CompanyList'
 import {
   Sidebar,
@@ -6,18 +11,32 @@ import {
   SidebarGroupContent,
   SidebarMenu,
 } from '@/components/ui/Sidebar'
+import { ChevronRight } from 'lucide-react'
 
 export default function SidebarNavigation() {
   return (
     <Sidebar>
       <SidebarContent className="gap-0">
-        <SidebarGroup>
-          <SidebarGroupContent>
-            <SidebarMenu>
-              <CompanyList />
-            </SidebarMenu>
-          </SidebarGroupContent>
-        </SidebarGroup>
+        <Collapsible
+          key="companies"
+          title="Companies"
+          defaultOpen
+          className="group/collapsible"
+        >
+          <SidebarGroup>
+            <CollapsibleTrigger className="group/label text-sm text-sidebar-foreground hover:bg-sidebar-accent hover:text-sidebar-accent-foreground flex items-center gap-2 mb-2">
+              <ChevronRight className="transition-transform group-data-[state=open]/collapsible:rotate-90" />
+              <div>Företag</div>
+            </CollapsibleTrigger>
+            <CollapsibleContent>
+              <SidebarGroupContent>
+                <SidebarMenu>
+                  <CompanyList />
+                </SidebarMenu>
+              </SidebarGroupContent>
+            </CollapsibleContent>
+          </SidebarGroup>
+        </Collapsible>
       </SidebarContent>
     </Sidebar>
   )
