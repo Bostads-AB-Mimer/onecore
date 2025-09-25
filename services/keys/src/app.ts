755e4ded2bf98f2d4fe8de334f72500b27c0a0ac
albinrm <albin.rm@hotmail.com>
2025-09-25 10:24:21 +0200

    feat: refactor api routes to use centrally defined types, added support to automatically produce swagger doc
    - Modified API response and request types in api-types.ts to include detailed schemas for keys, key loans, and key systems.
    - Refactored key-service routes to utilize new schema definitions for request validation and response formatting.
    - Introduced schema registration utility to dynamically register Zod

--- services/keys/src/app.ts
+++ services/keys/src/app.ts
@@ -1,4 +1,5 @@
 import Koa from 'koa'
+import KoaRouter from '@koa/router'
 import bodyParser from 'koa-bodyparser'
 import cors from '@koa/cors'
 import { logger, loggerMiddlewares } from '@onecore/utilities'
@@ -7,12 +8,16 @@ const app = new Koa()
 
 import { koaSwagger } from 'koa2-swagger-ui'
 
+import { routes as swagggerRoutes } from './services/swagger'
+
 app.use(cors())
 
 app.use(
   koaSwagger({
     routePrefix: '/swagger',
-    swaggerOptions: { url: '/swagger.json' },
+    swaggerOptions: {
+      url: '/swagger.json',
+    },
   })
 )
 
@@ -23,6 +28,10 @@ app.on('error', (err) => {
 app.use(bodyParser())
 app.use(loggerMiddlewares.pre)
 app.use(loggerMiddlewares.post)
+
+const publicRouter = new KoaRouter()
+swagggerRoutes(publicRouter)
+ app.use(publicRouter.routes())
 
 app.use(api.routes())
 app.use(api.allowedMethods())
