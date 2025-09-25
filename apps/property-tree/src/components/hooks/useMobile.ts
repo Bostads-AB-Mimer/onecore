4aef05cfa730a5818a640c562f7a57ac10262aab
Simon Kropp <31412347+simonkropp@users.noreply.github.com>
2025-09-25 11:52:05 +0200

    MIM-696: Update property tree general design (#60)
    align more with lovable + refactor

--- apps/property-tree/src/components/hooks/useMobile.ts
+++ apps/property-tree/src/components/hooks/useMobile.ts
@@ -1,16 +1,40 @@
 import * as React from 'react'
 
+const MOBILE_BREAKPOINT = 768
+const SMALL_MOBILE_BREAKPOINT = 480
+
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState(window.innerWidth < 768)
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
   React.useEffect(() => {
-    const handleResize = () => {
-      setIsMobile(window.innerWidth < 768)
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
     }
-
-    window.addEventListener('resize', handleResize)
-    return () => window.removeEventListener('resize', handleResize)
+    mql.addEventListener('change', onChange)
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    return () => mql.removeEventListener('change', onChange)
   }, [])
 
-  return isMobile
+  return !!isMobile
+}
+
+export function useIsSmallMobile() {
+  const [isSmallMobile, setIsSmallMobile] = React.useState<boolean | undefined>(
+    undefined
+  )
+
+  React.useEffect(() => {
+    const mql = window.matchMedia(
+      `(max-width: ${SMALL_MOBILE_BREAKPOINT - 1}px)`
+    )
+    const onChange = () => {
+      setIsSmallMobile(window.innerWidth < SMALL_MOBILE_BREAKPOINT)
+    }
+    mql.addEventListener('change', onChange)
+    setIsSmallMobile(window.innerWidth < SMALL_MOBILE_BREAKPOINT)
+    return () => mql.removeEventListener('change', onChange)
+  }, [])
+
+  return !!isSmallMobile
 }
