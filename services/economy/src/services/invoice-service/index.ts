796c0d8c3b9d41eb20a1b79ec759f650876aac7c
Anders Norrback Bornholm <anders@bornholm.se>
2025-09-30 16:22:27 +0200

    Fixed error in route (which is now obsolete anyway and should be removed)

--- services/economy/src/services/invoice-service/index.ts
+++ services/economy/src/services/invoice-service/index.ts
@@ -55,8 +55,7 @@ export const routes = (router: KoaRouter) => {
       ]
 
       const invoiceRows = await getInvoiceRows(
-        new Date(),
-        new Date(),
+        '2025',
         '001',
         invoices.map((v) => v.invoiceId)
       )
