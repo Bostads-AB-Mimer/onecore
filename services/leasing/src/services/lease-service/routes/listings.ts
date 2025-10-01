ab522f9affe1bec6c537ea6f7e6f5136f330af66
Linda Almqvist <34321514+lalmqvist@users.noreply.github.com>
2025-10-01 09:22:47 +0200

    UTH-233: hantera antal publiceringar och uthyrningsmetod (#58)
    * get listings that needs republishing and count no of previous listings
    * use attemptCount in UI
    * add new status for republished listings
    * remove unused statuses and type
    * refactor route to get vacant parkinspaces
    * rename local extension type for nr of listings
    * add area specific rules
    * move isSpecialResidentialArea and isSpecialProperty logit to leasing-service
    * Update listing count logic to count from last assigned listing instead of vacantFrom date
    
    ---------
    
    Co-authored-by: Johanna Grahn <johanna.grahn@iteam.se>

--- services/leasing/src/services/lease-service/routes/listings.ts
+++ services/leasing/src/services/lease-service/routes/listings.ts
@@ -272,6 +272,7 @@ export const routes = (router: KoaRouter) => {
       }
 
       const { listings } = parseResult.data
+
       const result = await listingAdapter.createMultipleListings(listings)
 
       if (!result.ok) {
