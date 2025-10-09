0a97a1113acdf09dca3505a7dbed05cedafe3916
Simon Kropp <31412347+simonkropp@users.noreply.github.com>
2025-10-09 10:33:58 +0200

    MIM-762: Fetch work orders on property id (#88)

--- core/src/adapters/work-order-adapter/generated/api-types.ts
+++ core/src/adapters/work-order-adapter/generated/api-types.ts
@@ -149,6 +149,45 @@ export interface paths {
       };
     };
   };
+  "/workOrders/propertyId/{propertyId}": {
+    /**
+     * Get work orders by property id
+     * @description Retrieves work orders based on the provided property id.
+     */
+    get: {
+      parameters: {
+        path: {
+          /** @description The property id to filter work orders. */
+          propertyId: string;
+        };
+      };
+      responses: {
+        /** @description Successfully retrieved work orders. */
+        200: {
+          content: {
+            "application/json": {
+              content?: {
+                workOrders?: components["schemas"]["WorkOrder"][];
+              };
+              /** @description Route metadata */
+              metadata?: Record<string, never>;
+            };
+          };
+        };
+        /** @description Internal server error. Failed to retrieve work orders. */
+        500: {
+          content: {
+            "application/json": {
+              /** @example Internal server error */
+              error?: string;
+              /** @description Route metadata */
+              metadata?: Record<string, never>;
+            };
+          };
+        };
+      };
+    };
+  };
   "/workOrders/xpand/residenceId/{residenceId}": {
     /**
      * Get work orders by residence id from xpand
@@ -167,6 +206,53 @@ export interface paths {
         path: {
           /** @description The residence id to filter work orders. */
           residenceId: string;
+        };
+      };
+      responses: {
+        /** @description Successfully retrieved work orders. */
+        200: {
+          content: {
+            "application/json": {
+              content?: {
+                workOrders?: components["schemas"]["XpandWorkOrder"][];
+              };
+              /** @description Route metadata */
+              metadata?: Record<string, never>;
+            };
+          };
+        };
+        /** @description Internal server error. Failed to retrieve work orders. */
+        500: {
+          content: {
+            "application/json": {
+              /** @example Internal server error */
+              error?: string;
+              /** @description Route metadata */
+              metadata?: Record<string, never>;
+            };
+          };
+        };
+      };
+    };
+  };
+  "/workOrders/xpand/propertyId/{propertyId}": {
+    /**
+     * Get work orders by property id from xpand
+     * @description Retrieves work orders from xpand based on the provided property id.
+     */
+    get: {
+      parameters: {
+        query?: {
+          /** @description The number of work orders to skip. */
+          skip?: number;
+          /** @description The number of work orders to fetch. */
+          limit?: number;
+          /** @description Whether to sort the work orders by ascending creation date. */
+          sortAscending?: boolean;
+        };
+        path: {
+          /** @description The property id to filter work orders. */
+          propertyId: string;
         };
       };
       responses: {
--- services/work-order/src/services/work-order-service/adapters/odoo-adapter/index.ts
+++ services/work-order/src/services/work-order-service/adapters/odoo-adapter/index.ts
@@ -43,6 +43,7 @@ const WORK_ORDER_FIELDS: string[] = [
   'call_between',
   'space_code',
   'equipment_code',
+  'estate_code',
   'rental_property_id',
   'create_date',
   'due_date',
@@ -141,6 +142,40 @@ export const getWorkOrdersByContactCode = async (
     const workOrders = odooWorkOrders.map((workOrder) => ({
       ...transformWorkOrder(workOrder),
       Messages: transformMessages(messagesById[workOrder.id]),
+    }))
+
+    return workOrders
+  } catch (error) {
+    console.error('Error fetching work orders:', error)
+    throw error
+  }
+}
+
+export const getWorkOrdersByPropertyId = async (
+  propertyId: string
+): Promise<WorkOrder[]> => {
+  try {
+    await odoo.connect()
+
+    const odooWorkOrders = await odoo.searchRead<OdooWorkOrder>(
+      'maintenance.request',
+      ['estate_code', '=', propertyId],
+      WORK_ORDER_FIELDS
+    )
+
+    const odooWorkOrderMessages = await odoo.searchRead<OdooWorkOrderMessage>(
+      'mail.message',
+      MESSAGE_DOMAIN(odooWorkOrders.map((workOrder) => workOrder.id)),
+      MESSAGE_FIELDS
+    )
+
+    const messagesById = groupBy(odooWorkOrderMessages, 'res_id')
+
+    const workOrders = odooWorkOrders.map((workOrder) => ({
+      ...transformWorkOrder(workOrder),
+      Messages: transformMessages(
+        messagesById[workOrder.id] satisfies OdooWorkOrderMessage[]
+      ),
     }))
 
     return workOrders
