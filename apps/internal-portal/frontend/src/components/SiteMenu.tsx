aa893d08a26a59c777306cd497253550f018e495
lindblomdavid <65215239+lindblomdavid@users.noreply.github.com>
2025-10-15 15:00:46 +0200

    changed from annonseringsinnehåll to annonsinnehall

--- apps/internal-portal/frontend/src/components/SiteMenu.tsx
+++ apps/internal-portal/frontend/src/components/SiteMenu.tsx
@@ -72,9 +72,9 @@ const SiteMenu = () => (
                 <Typography>Sökandeprofil</Typography>
               </MenuItem>
             </Link>
-            <Link to="/annonseringsinnehall">
+            <Link to="/annonsinnehall">
               <MenuItem onClick={popupState.close}>
-                <Typography>Annonseringsinnehåll</Typography>
+                <Typography>Annonsinnehåll</Typography>
               </MenuItem>
             </Link>
           </Menu>
