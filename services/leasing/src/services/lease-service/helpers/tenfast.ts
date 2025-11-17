a91b757557e6b47f16c370584f160635c225eefb
Andreas Lundqvist <momentiris@gmail.com>
2025-11-17 10:33:15 +0100

    what about this lease status calculation?

--- services/leasing/src/services/lease-service/helpers/tenfast.ts
+++ services/leasing/src/services/lease-service/helpers/tenfast.ts
@@ -2,25 +2,16 @@ import { Lease, LeaseStatus } from '@onecore/types'
 import { TenfastLease } from '../../../common/adapters/tenfast/schemas'
 
 const calculateLeaseStatus = (
-  lastDebitDateString: string,
-  startDateString: string
+  startDate: Date,
+  endDate: Date | null
 ): LeaseStatus => {
-  const leaseStartDate = new Date(startDateString)
-  const leaseLastDebitDate = new Date(lastDebitDateString)
+  // TODO: Verify this logic
   const today = new Date()
+  if (endDate && endDate >= today) return LeaseStatus.AboutToEnd
+  if (endDate && endDate < today) return LeaseStatus.Ended
+  if (startDate >= today) return LeaseStatus.Upcoming
 
-  leaseStartDate.setHours(0, 0, 0, 0)
-  leaseLastDebitDate.setHours(0, 0, 0, 0)
-  today.setHours(0, 0, 0, 0)
-
-  if (lastDebitDateString && leaseLastDebitDate >= today)
-    return LeaseStatus.AboutToEnd
-  else if (lastDebitDateString && leaseLastDebitDate < today)
-    return LeaseStatus.Ended
-  else if (leaseStartDate >= today) return LeaseStatus.Upcoming
-  else {
-    return LeaseStatus.Current
-  }
+  return LeaseStatus.Current
 }
 
 // tenantContactIds: string[] | undefined // Kanske vi vill ha external id
@@ -44,11 +35,7 @@ export function mapToOnecoreLease(lease: TenfastLease): Lease {
     leaseNumber: 'missing',
     leaseStartDate: lease.startDate,
     leaseEndDate: lease.endDate ?? undefined,
-    // calculateLeaseStatus will break because it needs properties tenfast doesnt have right now
-    status: calculateLeaseStatus(
-      lease.endDate?.toISOString() ?? '', // TODO: dunno if endDate is good here
-      lease.startDate.toISOString()
-    ),
+    status: calculateLeaseStatus(lease.startDate, lease.endDate),
     noticeGivenBy: undefined,
     noticeDate: undefined,
     noticeTimeTenant: undefined,
