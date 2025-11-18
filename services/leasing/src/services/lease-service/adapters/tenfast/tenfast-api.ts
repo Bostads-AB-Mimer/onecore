4430c79a291ec116c194d30fcd4fc78148cc818d
Johanna Grahn <johanna.grahn@iteam.se>
2025-11-18 16:05:52 +0100

    Changed to api key again

--- services/leasing/src/services/lease-service/adapters/tenfast/tenfast-api.ts
+++ services/leasing/src/services/lease-service/adapters/tenfast/tenfast-api.ts
@@ -1,63 +1,24 @@
 import config from '../../../../common/config'
-import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from 'axios'
+import axios, { AxiosRequestConfig, AxiosResponse } from 'axios'
 
 //todo: move to global config or handle error statuses in middleware
 axios.defaults.validateStatus = function (status) {
   return status >= 200 && status < 500 // override Axios throwing errors so that we can handle errors manually
 }
 
-const tenfastBaseUrl = config.tenfast.baseUrl
-
-let accessToken: string | undefined = undefined
-
-const getAccessToken = async () => {
-  const username = config.tenfast.username
-  const password = config.tenfast.password
-
-  const requestConfig = {
-    method: 'get',
-    url: `${tenfastBaseUrl}/v1/auth`,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    auth: {
-      username: username,
-      password: password,
-    },
-  }
-
-  const result = await axios(requestConfig)
-  return result.data.token
-}
-
-const createHeaders = (accessToken: string) => {
+const createHeaders = () => {
   const headers = {
     'Content-type': 'application/json',
-    'api-token': accessToken,
+    'api-token': config.tenfast.apiKey,
   }
 
   return headers
 }
 
-//Exported for testing purposes only
 export const request = async <T = any>(
   config: AxiosRequestConfig<any>
 ): Promise<AxiosResponse<T, any>> => {
-  if (!accessToken) {
-    accessToken = await getAccessToken()
-  }
+  config.headers = createHeaders()
 
-  try {
-    config.headers = createHeaders(accessToken ?? '')
-
-    return await axios(config)
-  } catch (error) {
-    const axiosErr = error as AxiosError
-    if (axiosErr.response?.status === 401) {
-      accessToken = await getAccessToken()
-      return await request(config)
-    }
-
-    throw error
-  }
+  return await axios(config)
 }
