54c56a916a91277dfda0e8df032dca0ffe34b30e
Sven Johansson <johansson.sven@gmail.com>
2025-11-19 12:52:57 +0100

    core: Add contacts-service adapter, config and health checks

--- core/src/services/swagger/index.ts
+++ core/src/services/swagger/index.ts
@@ -1,10 +1,21 @@
 import KoaRouter from '@koa/router'
 import swaggerJsdoc from 'swagger-jsdoc'
 import { swaggerSpec } from '../../swagger'
+import { OkapiRouter } from 'koa-okapi-router'
 
-export const routes = (router: KoaRouter) => {
+const swaggerOptions = swaggerJsdoc(swaggerSpec) as any
+
+export const routes = (router: KoaRouter, apiRouter: OkapiRouter) => {
   router.get('(.*)/swagger.json', async (ctx) => {
     ctx.set('Content-Type', 'application/json')
-    ctx.body = swaggerJsdoc(swaggerSpec)
+
+    const apiSpec = apiRouter.openapiJson() as any
+
+    swaggerOptions.paths = {
+      ...apiSpec.paths,
+      ...swaggerOptions.paths,
+    }
+
+    ctx.body = swaggerOptions
   })
 }
