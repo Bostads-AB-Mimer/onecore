faa115a08705a58908611295c3de01c6f98fd7db
lindblomdavid <65215239+lindblomdavid@users.noreply.github.com>
2025-11-20 16:46:07 +0100

    added componentpage with some dummydata

---
+++ apps/property-tree/src/components/component/ComponentHeader.tsx
@@ -0,0 +1,9 @@
+export default function ComponentHeader() {
+  return (
+    <div className="mb-6">
+      <h1 className="text-2xl sm:text-3xl font-bold">
+        Komponent - Värmepump
+      </h1>
+    </div>
+  )
+}
