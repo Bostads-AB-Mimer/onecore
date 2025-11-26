1efc8aa706f0637f12186906a574d169803e70c8
Andreas Lundqvist <momentiris@gmail.com>
2025-11-26 15:11:19 +0100

    leasing: remove seemingly unused tenfast schemas

--- services/leasing/src/services/lease-service/adapters/tenfast/schemas.ts
+++ services/leasing/src/services/lease-service/adapters/tenfast/schemas.ts
@@ -178,20 +178,6 @@ export const TenfastContractSchema = z.object({
 
 export type TenfastContract = z.infer<typeof TenfastContractSchema>
 
-export const TenfastRentArticleSchema = z.object({
-  includeInContract: z.boolean(),
-  _id: z.string(),
-  label: z.string(),
-  type: z.string(),
-  accountNr: z.string().nullable(),
-  createdAt: z.string(),
-  hyresvard: z.string(),
-  code: z.string(),
-  title: z.string(),
-})
-
-export type TenfastRentArticle = z.infer<typeof TenfastRentArticleSchema>
-
 export const TenfastLeaseTemplateSchema = z.object({
   _id: z.string(),
   hyresvardar: z.array(z.string()),
