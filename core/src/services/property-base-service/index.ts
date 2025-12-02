dc7a16c8cb2bb3773b71b9430061931190e76af4
lindblomdavid <65215239+lindblomdavid@users.noreply.github.com>
2025-12-02 14:12:39 +0100

    adjusted Added Missing ComponentCategory Schemas and changed params.data.typeId to match actual schema definition

--- core/src/services/property-base-service/index.ts
+++ core/src/services/property-base-service/index.ts
@@ -2538,7 +2538,7 @@ export const routes = (router: KoaRouter) => {
 
     try {
       const result = await propertyBaseAdapter.getComponentSubtypes(
-        params.data.typeId,
+        params.data.componentTypeId,
         params.data.page,
         params.data.limit
       )
@@ -3715,8 +3715,8 @@ export const routes = (router: KoaRouter) => {
     try {
       const result = await propertyBaseAdapter.createComponentInstallation({
         ...body.data,
-        installationDate: body.data.installationDate.toISOString(),
-        deinstallationDate: body.data.deinstallationDate?.toISOString(),
+        installationDate: body.data.installationDate,
+        deinstallationDate: body.data.deinstallationDate,
       })
 
       if (!result.ok) {
@@ -3795,8 +3795,8 @@ export const routes = (router: KoaRouter) => {
         id.data,
         {
           ...body.data,
-          installationDate: body.data.installationDate?.toISOString(),
-          deinstallationDate: body.data.deinstallationDate?.toISOString(),
+          installationDate: body.data.installationDate,
+          deinstallationDate: body.data.deinstallationDate,
         }
       )
 
