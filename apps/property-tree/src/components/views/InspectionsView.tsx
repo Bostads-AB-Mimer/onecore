852c70c2062560c44ce13ffc9b22b59f39dab6b9
protolarsson <stefan.larsson@prototyp.se>
2025-12-09 09:38:31 +0100

    Add route and placeholder file for Inspections view

---
+++ apps/property-tree/src/components/views/InspectionsView.tsx
@@ -0,0 +1,9 @@
+import { useState } from 'react'
+
+export default function AllInspectionsPage() {
+  return (
+    <div className="py-4 animate-in">
+      <div>PLACEHOLDER FOR INSPECTIONS VIEW</div>
+    </div>
+  )
+}
