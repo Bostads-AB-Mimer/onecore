073d1f854ddd9bacc5a592957ab7b02bd2ef80aa
Linda Almqvist <linda.almqvist@prototyp.se>
2025-12-17 13:31:04 +0100

    fix rebase errors

--- services/leasing/src/services/lease-service/tests/adapters/tenfast/factories/index.ts
+++
@@ -1,2 +0,0 @@
-export { TenfastLeaseTemplateFactory as tenfastTemplate } from './tenfast-template'
-export { TenfastRentalObjectByRentalObjectCodeResponseSchemaFactory as tenfastRentalObject } from './tenfast-rental-object'
