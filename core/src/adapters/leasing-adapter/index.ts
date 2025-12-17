a4b6cf909aac12d30a9031debd836a6444dc263d
Linda Almqvist <linda.almqvist@prototyp.se>
2025-12-17 14:19:08 +0100

    fix prettier error

--- core/src/adapters/leasing-adapter/index.ts
+++ core/src/adapters/leasing-adapter/index.ts
@@ -989,9 +989,10 @@ const preliminaryTerminateLease = async (
       if (status === 404) {
         return {
           ok: false,
-          err: errorType === 'tenant-not-found' || errorType === 'lease-not-found'
-            ? errorType
-            : 'lease-not-found',
+          err:
+            errorType === 'tenant-not-found' || errorType === 'lease-not-found'
+              ? errorType
+              : 'lease-not-found',
         }
       }
 
