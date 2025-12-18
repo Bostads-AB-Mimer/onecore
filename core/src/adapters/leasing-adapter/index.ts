44c3135cd34bd3c26fb7185b7e4e4716c4ea4f21
Linda Almqvist <linda.almqvist@prototyp.se>
2025-12-18 09:17:15 +0100

    remove redundant error

--- core/src/adapters/leasing-adapter/index.ts
+++ core/src/adapters/leasing-adapter/index.ts
@@ -656,10 +656,7 @@ const preliminaryTerminateLease = async (
   lastDebitDate: Date,
   desiredMoveDate: Date
 ): Promise<
-  AdapterResult<
-    any,
-    'tenant-not-found' | 'lease-not-found' | 'termination-failed' | 'unknown'
-  >
+  AdapterResult<any, 'lease-not-found' | 'termination-failed' | 'unknown'>
 > => {
   try {
     const response = await axios.post(
@@ -688,10 +685,7 @@ const preliminaryTerminateLease = async (
       if (status === 404) {
         return {
           ok: false,
-          err:
-            errorType === 'tenant-not-found' || errorType === 'lease-not-found'
-              ? errorType
-              : 'lease-not-found',
+          err: 'lease-not-found',
         }
       }
 
