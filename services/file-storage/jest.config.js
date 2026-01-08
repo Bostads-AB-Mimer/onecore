b39922a0e431b7e0a2f09974cabe910bd2457f3b
Simon Kropp <simon.kropp@prototyp.se>
2026-01-08 13:47:17 +0100

    tests + test configuration

--- services/file-storage/jest.config.js
+++ services/file-storage/jest.config.js
@@ -4,4 +4,13 @@ module.exports = {
   testEnvironment: 'node',
   modulePathIgnorePatterns: ['<rootDir>/build/'],
   extensionsToTreatAsEsm: ['.d.ts, .ts'],
+  testMatch: ['<rootDir>/**/__tests__/**/*.test.ts', '<rootDir>/**/*.test.ts'],
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+    '^@test/(.*)$': '<rootDir>/test/$1',
+  },
+  setupFiles: ['<rootDir>/.jest/common.ts'],
+  testTimeout: 10000,
+  forceExit: false,
+  detectOpenHandles: false,
 }
