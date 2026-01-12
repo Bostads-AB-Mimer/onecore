0acba979d376220baa1a769568a694bac9c20897
Sven Johansson <sven.johansson@iteam.se>
2026-01-12 13:20:57 +0100

    ContactRepository and query builder for xpand contacts

--- services/contacts/src/api.ts
+++ services/contacts/src/api.ts
@@ -3,19 +3,22 @@ import { makeOkapiRouter } from 'koa-okapi-router'
 import config from './common/config'
 
 import { routes as contactsRoutes } from './services/contacts-service'
-import { xpandContactsRepository } from './adapters/xpand/module'
-import { xpandDbClient } from './adapters/xpand/db'
+import { routes as healthRoutes } from './services/health-service'
+import { AppContext } from './context'
 
-const router = makeOkapiRouter(new KoaRouter(), {
-  openapi: {
-    info: { title: `ONECore ${config.applicationName}` },
-  },
-})
+const makeApi = (appContext: AppContext) => {
+  const router = makeOkapiRouter(new KoaRouter(), {
+    openapi: {
+      info: { title: `ONECore ${config.applicationName}` },
+    },
+  })
 
-const modules = {
-  contactsRepository: xpandContactsRepository(xpandDbClient()),
+  const { infrastructure, modules } = appContext
+
+  contactsRoutes(router, modules)
+  healthRoutes(router, infrastructure)
+
+  return router
 }
 
-contactsRoutes(router, modules)
-
-export default router
+export default makeApi
---
+++ services/contacts/src/domain/index.ts
@@ -0,0 +1,10 @@
+export type {
+  Contact,
+  PersonalDetails,
+  OrganisationDetails,
+  ContactIndividual,
+  ContactOrganisation,
+  ContactCode,
+  NationalIdNumber,
+  PhoneNumber,
+} from './contact'
