b4358be329e6f84756f959219dbbf8e280fd3cd6
Sebastian Salas <S3salas@gmail.com>
2026-01-14 14:33:05 +0100

    Name fixes

--- apps/property-tree/src/services/api/core/componentService.ts
+++ apps/property-tree/src/services/api/core/componentService.ts
@@ -261,10 +261,6 @@ export const componentService = {
   },
 
   async getImages(componentId: string): Promise<DocumentWithUrl[]> {
-    console.log(
-      '[componentService.getImages] Fetching images for:',
-      componentId
-    )
     // 1. Get document metadata from property service
     const { data, error } = await GET(
       '/api/documents/component-instances/{id}',
@@ -292,11 +288,6 @@ export const componentService = {
       })
     )
 
-    console.log(
-      '[componentService.getImages] Returning:',
-      documentsWithUrls.length,
-      'images'
-    )
     return documentsWithUrls as DocumentWithUrl[]
   },
 
