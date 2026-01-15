d204102a4f7dc30292c50d31570e5e8dc5fc34f0
Johanna Grahn <johanna.grahn@iteam.se>
2026-01-15 15:18:28 +0100

    Better tests

--- services/leasing/src/services/lease-service/tests/factories/tenfast-rental-object.ts
+++ services/leasing/src/services/lease-service/tests/factories/tenfast-rental-object.ts
@@ -5,25 +5,37 @@ import {
   TenfastRentalObjectByRentalObjectCodeResponse,
 } from '../../adapters/tenfast/schemas'
 
-export const TenfastRentalObjectByRentalObjectCodeResponseSchemaFactory =
-  Factory.define<TenfastRentalObjectByRentalObjectCodeResponse>(() => ({
-    records: [TenfastRentalObjectSchemaFactory.build()],
-    prev: null,
-    next: null,
-    totalCount: 1,
-  }))
+export const TenfastRentalObjectByRentalObjectCodeResponseFactory =
+  Factory.define<TenfastRentalObjectByRentalObjectCodeResponse>(
+    ({ sequence }) => ({
+      records: [
+        {
+          _id: '67eb8af5545c8f1195bef2e6' + sequence,
+          hyra: 287.17,
+          hyraVat: 0, // total moms pa hyran
+          hyraExcludingVat: 287.17, // hyran exklusive moms
+          hyror: TenfastInvoiceRowFactory.buildList(3),
+          externalId: `externalId-${sequence}`,
+        },
+      ],
+      prev: null,
+      next: null,
+      totalCount: 1,
+    })
+  )
 
-export const TenfastRentalObjectSchemaFactory =
-  Factory.define<TenfastRentalObject>(({ sequence }) => ({
-    _id: '67eb8af5545c8f1195bef2e6' + sequence,
-    externalId: '100' + sequence,
+export const TenfastRentalObjectFactory = Factory.define<TenfastRentalObject>(
+  ({ sequence }) => ({
+    _id: `rental-object-${sequence}`,
     hyra: 287.17,
-    hyraVat: 0, // total moms pa hyran
-    hyraExcludingVat: 287.17, // hyran exklusive moms
-    hyror: TenfastInvoiceRowSchemaFactory.buildList(3),
-  }))
+    hyraVat: 0,
+    hyraExcludingVat: 287.17,
+    hyror: [TenfastInvoiceRowFactory.build()],
+    externalId: 'externalId-1',
+  })
+)
 
-export const TenfastInvoiceRowSchemaFactory = Factory.define<TenfastInvoiceRow>(
+export const TenfastInvoiceRowFactory = Factory.define<TenfastInvoiceRow>(
   ({ sequence }) => ({
     amount: 115,
     vat: 0.25,
--- services/leasing/src/services/lease-service/tests/factories/tenfast.ts
+++ services/leasing/src/services/lease-service/tests/factories/tenfast.ts
@@ -1,12 +1,7 @@
 import { Factory } from 'fishery'
 
-import {
-  TenfastInvoiceRow,
-  TenfastLease,
-  TenfastRentalObject,
-  TenfastRentalObjectByRentalObjectCodeResponse,
-  TenfastTenant,
-} from '../../adapters/tenfast/schemas'
+import { TenfastLease, TenfastTenant } from '../../adapters/tenfast/schemas'
+import { TenfastRentalObjectFactory } from './tenfast-rental-object'
 
 export const TenfastLeaseFactory = Factory.define<TenfastLease>(
   ({ sequence }) => ({
@@ -85,46 +80,5 @@ export const TenfastTenantFactory = Factory.define<TenfastTenant>(
     borgenarer: [],
     firmatecknare: [],
     displayName: 'displayName-1',
-  })
-)
-
-export const TenfastRentalObjectFactory = Factory.define<TenfastRentalObject>(
-  ({ sequence }) => ({
-    _id: `rental-object-${sequence}`,
-    hyra: 287.17,
-    hyraVat: 0,
-    hyraExcludingVat: 287.17,
-    hyror: [TenfastInvoiceRowFactory.build()],
-    externalId: 'externalId-1',
-  })
-)
-
-export const TenfastRentalObjectByRentalObjectCodeResponseFactory =
-  Factory.define<TenfastRentalObjectByRentalObjectCodeResponse>(
-    ({ sequence }) => ({
-      records: [
-        {
-          _id: '67eb8af5545c8f1195bef2e6' + sequence,
-          hyra: 287.17,
-          hyraVat: 0, // total moms pa hyran
-          hyraExcludingVat: 287.17, // hyran exklusive moms
-          hyror: TenfastInvoiceRowFactory.buildList(3),
-          externalId: `externalId-${sequence}`,
-        },
-      ],
-      prev: null,
-      next: null,
-      totalCount: 1,
-    })
-  )
-
-export const TenfastInvoiceRowFactory = Factory.define<TenfastInvoiceRow>(
-  ({ sequence }) => ({
-    amount: 115,
-    vat: 0.25,
-    from: '2013-03',
-    article: '12334567' + sequence,
-    label: 'Hyra p-plats',
-    _id: sequence.toString(),
   })
 )
