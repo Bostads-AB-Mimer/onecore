21282ad10f0007ab3df079945caad8dee8956f09
Sven Johansson <sven.johansson@iteam.se>
2026-01-22 19:54:40 +0100

    contacts: Wire up the app dependencies up front

--- services/contacts/src/app.ts
+++ services/contacts/src/app.ts
@@ -5,44 +5,48 @@ import cors from '@koa/cors'
 import { koaSwagger } from 'koa2-swagger-ui'
 
 import makeApi from './api'
-import { errorHandler, logger, loggerMiddlewares } from '@onecore/utilities'
-import { makeAppContext } from './context'
+import { errorHandler, logger } from '@onecore/utilities'
+import { AppContext } from './context'
 
-const app = new Koa()
+export const makeApp = (appContext: AppContext) => {
+  const app = new Koa()
 
-app.use(cors())
-app.use(koaBody({ multipart: true, patchKoa: true }))
+  app.use(cors())
+  app.use(koaBody({ multipart: true, patchKoa: true }))
 
-app.on('error', (err) => {
-  logger.error(err, 'Uncaught error')
-})
-
-app.on('timeout', (err) => {
-  logger.error(err, 'Timeout')
-})
-
-app.use(loggerMiddlewares.pre)
-app.use(loggerMiddlewares.post)
-
-const appContext = makeAppContext()
-const api = makeApi(appContext)
-
-app.use(errorHandler())
-app.use(api.routes())
-
-app.use(
-  new KoaRouter()
-    .get(api.openapiJsonUrl, async (ctx) => {
-      ctx.body = api.openapiJson()
-    })
-    .routes()
-)
-
-app.use(
-  koaSwagger({
-    routePrefix: '/swagger',
-    swaggerOptions: { url: api.openapiJsonUrl },
+  app.on('error', (err) => {
+    logger.error(err, 'Uncaught error')
   })
-)
 
-export default app
+  app.on('timeout', (err) => {
+    logger.error(err, 'Timeout')
+  })
+
+  appContext.infrastructure.middlewares.forEach((mw) => {
+    app.use(mw)
+  })
+
+  const api = makeApi(appContext)
+
+  app.use(errorHandler())
+  app.use(api.routes())
+
+  app.use(
+    new KoaRouter()
+      .get(api.openapiJsonUrl, async (ctx) => {
+        ctx.body = api.openapiJson()
+      })
+      .routes()
+  )
+
+  app.use(
+    koaSwagger({
+      routePrefix: '/swagger',
+      swaggerOptions: { url: api.openapiJsonUrl },
+    })
+  )
+
+  return app
+}
+
+export default makeApp
