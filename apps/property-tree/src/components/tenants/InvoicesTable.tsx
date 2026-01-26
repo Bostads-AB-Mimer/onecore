52b4cd00d72d0c31257f7258e75b1f778d53d78b
Henrik Persson <henrik.persson@iteam.com>
2026-01-26 11:06:36 +0100

    Show expected loss in InvoicesTable

--- apps/property-tree/src/components/tenants/InvoicesTable.tsx
+++ apps/property-tree/src/components/tenants/InvoicesTable.tsx
@@ -437,6 +437,7 @@ export const InvoicesTable = (props: Props) => {
         {invoice.description && (
           <div className="mb-3 text-sm bg-background/50 rounded p-2">
             <span className="font-medium">Text:</span> {invoice.description}
+            {invoice.expectedLoss && <div>Befarad kundförlust</div>}
           </div>
         )}
         {invoice.credit && (
