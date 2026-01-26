37c02a886f580bd67911369b8aab22de3852ec6a
Linda Almqvist <linda.almqvist@prototyp.se>
2026-01-26 14:10:49 +0100

    update to adhere to mimer.nu api request to get leases

--- apps/internal-portal/frontend/src/pages/ParkingSpace/components/OfferRound.tsx
+++ apps/internal-portal/frontend/src/pages/ParkingSpace/components/OfferRound.tsx
@@ -206,6 +206,7 @@ const leaseStatusFormatMap: Record<LeaseStatus, string> = {
   [LeaseStatus.Upcoming]: 'Kommande',
   [LeaseStatus.AboutToEnd]: 'Uppsagt',
   [LeaseStatus.Ended]: 'Upphört',
+  [LeaseStatus.PreliminaryTerminated]: 'Preliminärt uppsagt',
 }
 
 const formatLeaseStatus = (v: LeaseStatus) => leaseStatusFormatMap[v]
