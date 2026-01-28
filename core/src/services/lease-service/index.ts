42d8efe943f70d5244bce7f51412012c2b1e9325
Linda Almqvist <linda.almqvist@prototyp.se>
2026-01-28 11:47:00 +0100

    update error handling

--- core/src/services/lease-service/index.ts
+++ core/src/services/lease-service/index.ts
@@ -270,7 +270,6 @@ export const routes = (router: KoaRouter) => {
           if (result.err === 'lease-not-found') {
             ctx.status = 404
             ctx.body = {
-              error: result.err,
               message: 'Lease not found',
               ...metadata,
             }
@@ -280,7 +279,6 @@ export const routes = (router: KoaRouter) => {
           if (result.err === 'tenant-email-missing') {
             ctx.status = 400
             ctx.body = {
-              error: result.err,
               message: 'Tenant missing valid email address',
               ...metadata,
             }
@@ -289,8 +287,7 @@ export const routes = (router: KoaRouter) => {
 
           ctx.status = 500
           ctx.body = {
-            error: result.err,
-            message: 'Failed to preliminary terminate lease',
+            error: 'Failed to preliminary terminate lease',
             ...metadata,
           }
           return
