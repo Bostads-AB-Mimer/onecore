eaba581e04be595c4f41c8965313b45b0b50e8c0
Sebastian Salas <S3salas@gmail.com>
2026-02-02 15:17:18 +0100

    lease-search-adapter fixes

--- libs/types/src/enums.ts
+++ libs/types/src/enums.ts
@@ -5,6 +5,13 @@ enum LeaseStatus {
   Ended = 3, //Upphört
 }
 
+const LeaseStatusLabel: Record<LeaseStatus, string> = {
+  [LeaseStatus.Current]: 'Gällande',
+  [LeaseStatus.Upcoming]: 'Kommande',
+  [LeaseStatus.AboutToEnd]: 'Uppsagt',
+  [LeaseStatus.Ended]: 'Upphört',
+}
+
 enum ParkingSpaceType {
   WarmGarage,
   ColdGarage,
@@ -87,6 +94,7 @@ enum WaitingListType {
 
 export {
   LeaseStatus,
+  LeaseStatusLabel,
   ParkingSpaceType,
   ParkingSpaceApplicationCategory,
   PaymentStatus,
