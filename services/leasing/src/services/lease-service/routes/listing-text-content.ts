9ccda2a0064254e4042e0b3755b10395756218bd
lindblomdavid <65215239+lindblomdavid@users.noreply.github.com>
2026-02-05 16:20:38 +0100

    Update services/leasing/src/services/lease-service/routes/listing-text-content.ts
    Co-authored-by: Linda Almqvist <34321514+lalmqvist@users.noreply.github.com>

--- services/leasing/src/services/lease-service/routes/listing-text-content.ts
+++ services/leasing/src/services/lease-service/routes/listing-text-content.ts
@@ -273,7 +273,6 @@ export const routes = (router: KoaRouter) => {
     const result = await listingTextContentAdapter.remove(rentalObjectCode)
 
     if (!result.ok) {
-      // Check if this is a "not found" error
       if (result.err.message.includes('not found')) {
         ctx.status = 404
         ctx.body = { error: result.err.message, ...metadata }
