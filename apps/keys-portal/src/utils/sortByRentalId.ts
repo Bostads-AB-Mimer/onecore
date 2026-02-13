52aeb1ab4bf3530fa2799bd1a8606a0c15dd80f6
crewiz <s3salas@gmail.com>
2026-02-13 09:51:49 +0100

    a little file

--- apps/keys-portal/src/utils/sortByRentalId.ts
+++ apps/keys-portal/src/utils/sortByRentalId.ts
@@ -1,7 +1,5 @@
 export function sortByRentalId<T extends { rentalId: string }>(
   items: T[]
 ): T[] {
-  return [...items].sort((a, b) =>
-    a.rentalId.localeCompare(b.rentalId, 'sv')
-  )
+  return [...items].sort((a, b) => a.rentalId.localeCompare(b.rentalId, 'sv'))
 }
