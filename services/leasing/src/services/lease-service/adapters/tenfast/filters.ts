160453fe28b2e8fb5121726f9d77aa665def2274
Andreas Lundqvist <31646645+momentiris@users.noreply.github.com>
2026-02-18 16:16:17 +0100

    Fix/missing status prel terminated (#368)
    * add preliminary-terminated status to filter
    
    * Update tenfast-adapter.ts
    
    * Update filters.ts
    
    * update filters
    
    ---------
    
    Co-authored-by: Linda Almqvist <linda.almqvist@prototyp.se>

--- services/leasing/src/services/lease-service/adapters/tenfast/filters.ts
+++ services/leasing/src/services/lease-service/adapters/tenfast/filters.ts
@@ -40,7 +40,7 @@ function isCurrentLease(l: TenfastLease, now: Date) {
     l.startDate < now &&
     (!l.endDate || l.endDate > now) &&
     !isPreliminaryTerminated(l) &&
-    !l.cancellation.handledAt
+    !l.cancellation.cancelled
   )
 }
 
@@ -49,7 +49,7 @@ function isUpcomingLease(l: TenfastLease, now: Date) {
 }
 
 function isAboutToEndLease(l: TenfastLease, now: Date) {
-  return l.endDate && l.endDate >= now && !!l.cancellation.handledAt
+  return l.endDate && l.endDate >= now && !!l.cancellation.cancelled
 }
 
 function isEndedLease(l: TenfastLease, now: Date) {
@@ -57,5 +57,5 @@ function isEndedLease(l: TenfastLease, now: Date) {
 }
 
 export const isPreliminaryTerminated = (lease: TenfastLease): boolean => {
-  return !!lease.simplesignTermination?.sentAt && !lease.cancellation.handledAt
+  return !!lease.simplesignTermination?.sentAt && !lease.cancellation.cancelled
 }
