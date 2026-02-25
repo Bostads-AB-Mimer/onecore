869096c389195552bf7913cd9f5852e390da7c7b
Johanna Månsson Grahn <johanna.grahn@iteam.se>
2026-02-25 09:31:56 +0100

    Feat: UTH-339-Desktop-Menu (#380)
    * The menu is always visible and placed horizontally on desktop

--- apps/internal-portal/frontend/src/components/SiteMenu.tsx
+++ apps/internal-portal/frontend/src/components/SiteMenu.tsx
@@ -1,87 +1,103 @@
-import { IconButton, MenuItem, Menu, Typography, Backdrop } from '@mui/material'
+import {
+  Box,
+  IconButton,
+  MenuItem,
+  Menu,
+  Typography,
+  Button,
+  Backdrop,
+} from '@mui/material'
 import PopupState, { bindTrigger, bindMenu } from 'material-ui-popup-state'
 import MenuIcon from '@mui/icons-material/Menu'
 import CloseIcon from '@mui/icons-material/Close'
 import { Link } from 'react-router-dom'
+import { useTheme } from '@mui/material/styles'
+import useMediaQuery from '@mui/material/useMediaQuery'
 
-const SiteMenu = () => (
-  <PopupState variant="popover" popupId="demo-popup-menu">
-    {(popupState) => (
-      <>
-        <IconButton {...bindTrigger(popupState)} sx={{ padding: 0 }}>
-          <MenuIcon />
-        </IconButton>
+const menuItems = [
+  { label: 'Bilplatser', to: '/bilplatser' },
+  { label: 'Publicera bilplatser', to: '/bilplatser/publicera' },
+  { label: 'Sök bilplats', to: '/sok-bilplats' },
+  // { label: 'Annonsinnehåll', to: '/annonsinnehall' },
+  { label: 'Sökandeprofil', to: '/sokandeprofil' },
+]
 
-        <Backdrop open={popupState.isOpen} onClick={popupState.close}>
-          <Menu
-            {...bindMenu(popupState)}
-            anchorOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'right',
-            }}
-            elevation={0}
-            sx={{ top: -16, left: 20 }}
-          >
-            <IconButton
-              onClick={popupState.close}
-              sx={{ position: 'absolute', top: 5, right: 10 }}
+const SiteMenu = () => {
+  const theme = useTheme()
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'))
+
+  if (isMobile) {
+    return (
+      <PopupState variant="popover" popupId="site-menu-mobile">
+        {(popupState) => (
+          <>
+            <Box
+              sx={{
+                display: 'flex',
+                alignItems: 'center',
+                background: '#fff',
+              }}
             >
-              <CloseIcon />
-            </IconButton>
-
-            <MenuItem
+              <IconButton {...bindTrigger(popupState)} sx={{ padding: 0 }}>
+                <MenuIcon />
+              </IconButton>
+            </Box>
+            <Backdrop
+              open={popupState.isOpen}
               onClick={popupState.close}
-              sx={{ width: '200px' }}
-              disabled
+              sx={{ zIndex: 1201 }}
             >
-              <Typography variant="h1">OneCore</Typography>
-            </MenuItem>
-            <Link to="https://onecore.mimer.nu">
-              <MenuItem onClick={popupState.close}>
-                <Typography>OneCore startsida</Typography>
-              </MenuItem>
-            </Link>
-            <Link to="/bilplatser">
-              <MenuItem onClick={popupState.close}>
-                <Typography>Bilplatser</Typography>
-              </MenuItem>
-            </Link>
-            <Link to="/bilplatser/publicera">
-              <MenuItem onClick={popupState.close}>
-                <Typography>Publicera bilplatser</Typography>
-              </MenuItem>
-            </Link>
-            <Link to="/sok-bilplats">
-              <MenuItem onClick={popupState.close}>
-                <Typography>Sök bilplats</Typography>
-              </MenuItem>
-            </Link>
-            {/* 
-            <Link to="/materialval">
-              <MenuItem onClick={popupState.close}>
-                <Typography>Materialval</Typography>
-              </MenuItem>
-            </Link> */}
+              <Menu
+                {...bindMenu(popupState)}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                elevation={0}
+                sx={{ top: -16, left: 20 }}
+              >
+                <IconButton
+                  onClick={popupState.close}
+                  sx={{ position: 'absolute', top: 5, right: 10 }}
+                >
+                  <CloseIcon />
+                </IconButton>
+                {menuItems.map((item) => (
+                  <Link key={item.to} to={item.to}>
+                    <MenuItem onClick={popupState.close}>
+                      <Typography>{item.label}</Typography>
+                    </MenuItem>
+                  </Link>
+                ))}
+              </Menu>
+            </Backdrop>
+          </>
+        )}
+      </PopupState>
+    )
+  }
 
-            <Link to="/sokandeprofil">
-              <MenuItem onClick={popupState.close}>
-                <Typography>Sökandeprofil</Typography>
-              </MenuItem>
-            </Link>
-            {/* <Link to="/annonsinnehall">
-              <MenuItem onClick={popupState.close}>
-                <Typography>Annonsinnehåll</Typography>
-              </MenuItem>
-            </Link> */}
-          </Menu>
-        </Backdrop>
-      </>
-    )}
-  </PopupState>
-)
+  // Desktop: horisontell meny
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        alignItems: 'center',
+        gap: 2,
+        background: '#fff',
+      }}
+    >
+      {menuItems.map((item) => (
+        <Button key={item.to} component={Link} to={item.to} color="inherit">
+          {item.label}
+        </Button>
+      ))}
+    </Box>
+  )
+}
 
 export default SiteMenu
