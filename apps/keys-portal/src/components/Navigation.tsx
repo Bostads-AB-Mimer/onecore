27de0cb8edb1471545ccfc4296a33d8cd3a29e09
albinrm <albin.rm@hotmail.com>
2026-03-04 08:56:05 +0100

    Lånehantering

--- apps/keys-portal/src/components/Navigation.tsx
+++ apps/keys-portal/src/components/Navigation.tsx
@@ -44,7 +44,7 @@ export function Navigation({ ...props }: React.ComponentProps<typeof Sidebar>) {
         <SidebarGroup>
           <SidebarGroupContent>
             <SidebarMenu>
-              {/* In- och utlämning */}
+              {/* Lånehantering */}
               <SidebarMenuItem>
                 <SidebarMenuButton
                   asChild
@@ -52,7 +52,7 @@ export function Navigation({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 >
                   <Link to="/KeyLoan">
                     <KeyRound className="h-4 w-4" />
-                    <span>In- och utlämning</span>
+                    <span>Lånehantering</span>
                   </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
