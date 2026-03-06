51371b07f9c0d5115d51a103ab6675d91cbb1143
Johanna Grahn <johanna.grahn@iteam.se>
2026-03-06 15:23:38 +0100

    Feat: AVTAL-66 Endpoints for Aktiv Bo
    Aktivbo needs two endpoints
    
    * Upcoming Move Ins will return a list of active apartment leases starting during the current month. The data will also include info on the customer and info on the rental object

--- libs/types/src/response-types/index.ts
+++ libs/types/src/response-types/index.ts
@@ -1,3 +1,4 @@
 export * from './detailed-applicant'
 export * from './offer-with-rentalObjectCode'
 export * from './detailed-offer'
+export * from './lease-with-contact-and-rental-object-info'
--- libs/types/src/types.ts
+++ libs/types/src/types.ts
@@ -45,6 +45,8 @@ interface Contact {
   housingWaitingList?: WaitingList
   storageWaitingList?: WaitingList
   specialAttention?: boolean
+  protectedIdentity: boolean
+  deceased: boolean
 }
 
 type NonEmptyArray<T> = [T, ...T[]]
@@ -287,6 +289,8 @@ interface RentalPropertyInfo {
   type: string
   property: ApartmentInfo | CommercialSpaceInfo | ParkingSpaceInfo
   maintenanceUnits?: MaintenanceUnitInfo[]
+  districtCode: string
+  district: string
 }
 
 interface ApartmentInfo {
