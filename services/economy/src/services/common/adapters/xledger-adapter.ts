c04af9f463164b9c81b59020ecd3ba5e0df6a7de
Henrik Persson <henrik.persson@iteam.com>
2026-03-10 09:37:09 +0100

    Put reference first in invoice rows

--- services/economy/src/services/common/adapters/xledger-adapter.ts
+++ services/economy/src/services/common/adapters/xledger-adapter.ts
@@ -1127,7 +1127,7 @@ export const submitMiscellaneousInvoice = async (
     articleId: 'Vår referens',
     articleName: invoice.reference,
   }
-  const rowsWithReference = invoice.invoiceRows.concat(referenceRow)
+  const rowsWithReference = [referenceRow, ...invoice.invoiceRows]
 
   const nodes = rowsWithReference.map(
     (ir, index) => gql`
