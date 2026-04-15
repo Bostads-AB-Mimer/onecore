c723cfe5723e8848daa1b1334782daaf94c1101e
Anders Norrback Bornholm <anders@bornholm.se>
2026-04-15 15:04:49 +0200

    Started implementing marking invoices as exported

--- services/economy/src/scripts/export-rental-invoices-accounting.ts
+++ services/economy/src/scripts/export-rental-invoices-accounting.ts
@@ -6,7 +6,31 @@ import { exportRentalInvoicesAccounting } from '@src/services/invoice-service/se
  * the ERP system (Xledger)
  */
 const exportRentalInvoicesScript = async () => {
-  await exportRentalInvoicesAccounting('')
+  const errors: { invoiceNumber: string; error: string }[] = []
+  let keepGoing = true
+
+  do {
+    const exportResults = await exportRentalInvoicesAccounting('')
+    console.log(`Exported ${exportResults.invoices?.length} invoices`)
+
+    if (exportResults.errors) {
+      errors.push(...exportResults.errors)
+      console.log('Errors:')
+      console.log(
+        exportResults.errors
+          .map((error) => {
+            return `${error.invoiceNumber}: ${error.error}`
+          })
+          .join('\n')
+      )
+    }
+
+    if (exportResults.invoices?.length <= 0) {
+      keepGoing = false
+    }
+  } while (keepGoing)
+
+  console.log('Export complete')
 }
 
 exportRentalInvoicesScript()
