d5aa4d4f41cdc153e8375fd7e9986758fa2769ee
albinrm <109419363+albinrm@users.noreply.github.com>
2026-04-27 13:00:51 +0200

    adds support to recieve skadjur work orders, sorts these out to send … (#435)
    * adds support to recieve skadjur work orders, sorts these out to send them to Skadedjurssamordnare
    
    * prettier
    
    * remove optional
    
    Co-authored-by: Johannes Karlsson <47555484+johanneskarlsson@users.noreply.github.com>
    
    * locationcode required
    
    Co-authored-by: Johannes Karlsson <47555484+johanneskarlsson@users.noreply.github.com>
    
    * part of buildingcode required
    
    Co-authored-by: Johannes Karlsson <47555484+johanneskarlsson@users.noreply.github.com>
    
    * normalize inputs to lägenhet and, perserve inital input for the title
    
    ---------
    
    Co-authored-by: Johannes Karlsson <47555484+johanneskarlsson@users.noreply.github.com>

--- services/work-order/src/services/work-order-service/adapters/odoo-adapter/index.ts
+++ services/work-order/src/services/work-order-service/adapters/odoo-adapter/index.ts
@@ -266,7 +266,14 @@ export const createWorkOrder = async (
 ): Promise<AdapterResult<number, unknown>> => {
   try {
     await odoo.connect()
-    const maintenanceTeamId = await getMaintenanceTeamId('Kundcenter')
+    const isPestWorkOrder = details.Rows.some(
+      (row) =>
+        row.PartOfBuildingCode.trim() === 'SD' ||
+        row.PartOfBuildingCode.trim() === 'DJUR'
+    )
+    const maintenanceTeamId = await getMaintenanceTeamId(
+      isPestWorkOrder ? 'Skadedjurssamordnare' : 'Kundcenter'
+    )
 
     const newRentalPropertyRecord =
       await createRentalPropertyRecord(rentalPropertyInfo)
@@ -397,14 +404,22 @@ const createWorkOrderRecord = async (
   details: CreateWorkOrderDetails
 ): Promise<number> => {
   try {
-    const supportedSpaceCodes = z.enum(['TV', 'BWC', 'KÖ'])
-    const captionForSpace: Record<
+    const supportedSpaceCodes = z.enum(['TV', 'BWC', 'KÖ', 'LGH', 'FÖR', 'KÄL'])
+    const odooSpaceCategory: Record<
       z.infer<typeof supportedSpaceCodes>,
       string
     > = {
       TV: 'Tvättstuga',
       BWC: 'Lägenhet',
       KÖ: 'Lägenhet',
+      LGH: 'Lägenhet',
+      FÖR: 'Lägenhet',
+      KÄL: 'Lägenhet',
+    }
+    const spaceInTitle: Record<z.infer<typeof supportedSpaceCodes>, string> = {
+      ...odooSpaceCategory,
+      FÖR: 'Förråd',
+      KÄL: 'Källare',
     }
 
     const uniqueSpaceCodes: z.infer<typeof supportedSpaceCodes>[] = []
@@ -414,7 +429,7 @@ const createWorkOrderRecord = async (
 
     details.Rows.forEach((row) => {
       const spaceCodeParseResult = supportedSpaceCodes.safeParse(
-        row.LocationCode
+        row.LocationCode.trim()
       )
       if (!spaceCodeParseResult.success) {
         throw new Error('Unsupported location code')
@@ -424,18 +439,19 @@ const createWorkOrderRecord = async (
       if (!uniqueSpaceCodes.includes(spaceCode)) {
         uniqueSpaceCodes.push(spaceCode)
 
-        if (!uniqueSpaceCaptions.includes(captionForSpace[spaceCode])) {
-          uniqueSpaceCaptions.push(captionForSpace[spaceCode])
+        if (!uniqueSpaceCaptions.includes(odooSpaceCategory[spaceCode])) {
+          uniqueSpaceCaptions.push(odooSpaceCategory[spaceCode])
         }
       }
 
-      if (!uniqueEquipmentCodes.includes(row.PartOfBuildingCode)) {
-        uniqueEquipmentCodes.push(row.PartOfBuildingCode)
+      const trimmedPartOfBuildingCode = row.PartOfBuildingCode.trim()
+      if (!uniqueEquipmentCodes.includes(trimmedPartOfBuildingCode)) {
+        uniqueEquipmentCodes.push(trimmedPartOfBuildingCode)
       }
 
       if (details.Rows.length > 1) {
         descriptions.push(
-          `${transformEquipmentCode(row.PartOfBuildingCode)}: ${row.Description}`
+          `${transformEquipmentCode(trimmedPartOfBuildingCode)}: ${row.Description}`
         )
       } else {
         descriptions.push(row.Description)
@@ -443,9 +459,12 @@ const createWorkOrderRecord = async (
     })
 
     const name =
-      uniqueEquipmentCodes.length > 1
-        ? `Felanmälda vitvaror - ${uniqueEquipmentCodes.map(transformEquipmentCode).join(', ')}`
-        : `Felanmäld ${captionForSpace[uniqueSpaceCodes[0]]} - ${transformEquipmentCode(uniqueEquipmentCodes[0])}`
+      uniqueEquipmentCodes.includes('SD') ||
+      uniqueEquipmentCodes.includes('DJUR')
+        ? `Felanmäld Skadedjur - ${[...new Set(uniqueSpaceCodes.map((c) => spaceInTitle[c]))].join(', ')}`
+        : uniqueEquipmentCodes.length > 1
+          ? `Felanmälda vitvaror - ${uniqueEquipmentCodes.map(transformEquipmentCode).join(', ')}`
+          : `Felanmäld ${spaceInTitle[uniqueSpaceCodes[0]]} - ${transformEquipmentCode(uniqueEquipmentCodes[0])}`
 
     return await odoo.create('maintenance.request', {
       rental_property_id: rentalPropertyRecord.toString(),
@@ -463,8 +482,10 @@ const createWorkOrderRecord = async (
       master_key: details.AccessOptions.Type === 0,
       space_caption: uniqueSpaceCaptions.join(', '),
       maintenance_team_id: maintenanceTeamId,
-      maintenance_request_category_id:
-        await getMaintenanceRequestCategoryId(uniqueSpaceCaptions),
+      maintenance_request_category_id: await getMaintenanceRequestCategoryId(
+        uniqueSpaceCaptions,
+        uniqueEquipmentCodes
+      ),
       creation_origin: 'mimer-nu',
     })
   } catch (error) {
@@ -491,10 +512,19 @@ const getMaintenanceTeamId = async (teamName: string): Promise<number> => {
 }
 
 const getMaintenanceRequestCategoryId = async (
-  uniqueSpaceCaptions: string[]
+  uniqueSpaceCaptions: string[],
+  uniqueEquipmentCodes: string[]
 ): Promise<number> => {
   try {
-    if (uniqueSpaceCaptions.includes('Tvättstuga')) {
+    if (
+      uniqueEquipmentCodes.includes('SD') ||
+      uniqueEquipmentCodes.includes('DJUR')
+    ) {
+      const categories = await odoo.search('maintenance.request.category', {
+        name: 'Skadedjur',
+      })
+      return categories[0]
+    } else if (uniqueSpaceCaptions.includes('Tvättstuga')) {
       const categories = await odoo.search('maintenance.request.category', {
         name: 'Tvättstuga',
       })
