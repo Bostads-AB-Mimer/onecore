03aef6ead4f8d200e8a4699b7bba3479c9f727d5
albinrm <albin.rm@hotmail.com>
2026-06-15 14:50:51 +0200

    proper db setup

--- services/communication/knexfile.js
+++ services/communication/knexfile.js
@@ -1,66 +1,35 @@
-// Update with your config settings.
-
 require('dotenv').config()
+
+const baseConfig = {
+  client: 'mssql',
+  connection: {
+    host: process.env.COMMUNICATION_DATABASE__HOST,
+    database: process.env.COMMUNICATION_DATABASE__DATABASE,
+    user: process.env.COMMUNICATION_DATABASE__USER,
+    password: process.env.COMMUNICATION_DATABASE__PASSWORD,
+    port: parseInt(process.env.COMMUNICATION_DATABASE__PORT),
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+  },
+}
 
 /**
  * @type { Object.<string, import("knex").Knex.Config> }
  */
-
 module.exports = {
-  client: 'mssql',
-  dev: {
-    client: 'mssql',
-    connection: {
-      host: process.env.COMMUNICATION_DATABASE__HOST,
-      database: process.env.COMMUNICATION_DATABASE__DATABASE,
-      user: process.env.COMMUNICATION_DATABASE__USER,
-      password: process.env.COMMUNICATION_DATABASE__PASSWORD,
-      port: parseInt(process.env.COMMUNICATION_DATABASE__PORT),
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
+  development: {
+    ...baseConfig,
     seeds: {
       directory: './seeds/dev',
     },
   },
   test: {
-    client: 'mssql',
+    ...baseConfig,
     connection: {
-      host: process.env.COMMUNICATION_DATABASE__HOST,
+      ...baseConfig.connection,
       database: 'communication-test',
-      user: process.env.COMMUNICATION_DATABASE__USER,
-      password: process.env.COMMUNICATION_DATABASE__PASSWORD,
-      port: parseInt(process.env.COMMUNICATION_DATABASE__PORT),
-    },
-    migrations: {
-      tableName: 'knex_migrations',
     },
   },
-  ci: {
-    client: 'mssql',
-    connection: {
-      host: process.env.COMMUNICATION_DATABASE__HOST,
-      database: process.env.COMMUNICATION_DATABASE__DATABASE,
-      user: process.env.COMMUNICATION_DATABASE__USER,
-      password: process.env.COMMUNICATION_DATABASE__PASSWORD,
-      port: parseInt(process.env.COMMUNICATION_DATABASE__PORT),
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
-  production: {
-    client: 'mssql',
-    connection: {
-      host: process.env.COMMUNICATION_DATABASE__HOST,
-      database: process.env.COMMUNICATION_DATABASE__DATABASE,
-      user: process.env.COMMUNICATION_DATABASE__USER,
-      password: process.env.COMMUNICATION_DATABASE__PASSWORD,
-      port: parseInt(process.env.COMMUNICATION_DATABASE__PORT),
-    },
-    migrations: {
-      tableName: 'knex_migrations',
-    },
-  },
+  production: baseConfig,
 }
