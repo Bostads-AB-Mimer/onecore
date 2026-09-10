5c163c4b437fd8d27cd79dcd4a83408818223c54
lindblomdavid <65215239+lindblomdavid@users.noreply.github.com>
2026-09-10 10:40:27 +0200

    prettier

--- apps/property-tree/src/services/api/core/generated/api-types.ts
+++ apps/property-tree/src/services/api/core/generated/api-types.ts
@@ -2803,11 +2803,7 @@ export interface paths {
         query: {
           /** @description The type of the identifier used to fetch work order data. */
           handler:
-            | 'rentalObjectId'
-            | 'leaseId'
-            | 'pnr'
-            | 'phoneNumber'
-            | 'contactCode'
+            'rentalObjectId' | 'leaseId' | 'pnr' | 'phoneNumber' | 'contactCode'
         }
         path: {
           /** @description The identifier value for fetching work order data. */
@@ -14889,12 +14885,7 @@ export interface components {
         toAddress: string
         /** @enum {string} */
         status:
-          | 'pending'
-          | 'sent'
-          | 'delivered'
-          | 'failed'
-          | 'bounced'
-          | 'received'
+          'pending' | 'sent' | 'delivered' | 'failed' | 'bounced' | 'received'
         /** Format: date-time */
         statusUpdatedAt: string
         externalMessageId: string | null
@@ -14937,12 +14928,7 @@ export interface components {
         toAddress: string
         /** @enum {string} */
         status:
-          | 'pending'
-          | 'sent'
-          | 'delivered'
-          | 'failed'
-          | 'bounced'
-          | 'received'
+          'pending' | 'sent' | 'delivered' | 'failed' | 'bounced' | 'received'
         /** Format: date-time */
         statusUpdatedAt: string
         externalMessageId: string | null
