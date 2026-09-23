f9ddb8bd427c655ef441debd33dc94e2cd320326
Henrik Persson <henrik.persson@iteam.com>
2026-09-23 09:20:23 +0200

    canDelete is apparently undefined sometimes

--- services/economy/src/common/adapters/tenfast/schemas.ts
+++ services/economy/src/common/adapters/tenfast/schemas.ts
@@ -105,7 +105,7 @@ export const TenfastLeaseSchema = z.object({
   hyresobjekt: z.array(TenfastRentalPropertySchema).min(1),
   reference: z.number(),
   invitationsToRegister: z.array(z.any()),
-  canDelete: z.boolean(),
+  canDelete: z.boolean().optional(),
   depositState: z.array(z.any()),
 })
 export const TenfastInvoiceStateSchema = z.enum([
@@ -183,7 +183,7 @@ export const TenfastInvoiceSchema = z.object({
       externalId: z.string(),
       reference: z.number(),
       stage: z.string(),
-      canDelete: z.boolean(),
+      canDelete: z.boolean().optional(),
       canVoid: z.boolean(),
       id: z.string(),
     })
